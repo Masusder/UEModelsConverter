2f8daa84340924da59346f29139a3ad0b3e1bdf0
Masusder <Mateusz.rutkowski076@gmail.com>
2024-02-25 14:09:33 +0100

    Merged UEModelsConverter with assets parser

--- UEModelsConverter.py
+++ UEModelsConverter.py
@@ -5,22 +5,25 @@ import shutil
 from io_import_scene_unreal_psa_psk_280 import *
 
 # Specify the root directory
-root_directory = r"D:\CProjects\UEModelsConverter"
+root_directory = r"C:\Users\mateu\Downloads\CodeProjects\AssetsParser\UEModelsConverter"
 
 # Specify the input directory
-input_directory = r"D:\CProjects\UEModelsConverter\Input"
+input_directory = r"C:\Users\mateu\Downloads\CodeProjects\AssetsParser\UEModelsConverter\Input"
 
 # Specify the input directory for JSON files
-input_mapping_directory = os.path.join(root_directory, "InputMapping")
+input_mapping_directory = r"C:\Users\mateu\Downloads\CodeProjects\AssetsParser\Output\ModelsData"
 
 # Specify the output directory
-output_directory = r"D:\CProjects\UEModelsConverter\Output"
+output_directory = r"C:\Users\mateu\Downloads\CodeProjects\AssetsParser\UEModelsConverter\Output"
 
 # Specify the log directory
 log_directory = os.path.join(root_directory, "Logs")
 
 # Specify the log file path
 log_file_path = os.path.join(log_directory, "log.txt")
+
+# Path for logs check
+log_check_file_path = r"D:\dbd-info-dev\assets"
 
 # Clear the log file on script start, old log will be lost
 with open(log_file_path, 'w') as log_file:
@@ -30,6 +33,18 @@ with open(log_file_path, 'w') as log_file:
 def log_to_file(message):
     with open(log_file_path, 'a') as log_file:
         log_file.write(message + '\n')
+
+def log_textures_to_file(texture_paths):
+    # Check if texture paths exist in the directory
+    for texture_path in texture_paths:
+        # Remove the initial "/assets/" part from the texture path
+        texture_relative_path = texture_path.replace("/assets/", "")
+        check_texture_path = os.path.join(log_check_file_path, texture_relative_path)
+        if not os.path.exists(check_texture_path):
+            # Log missing texture paths to a file
+            message = f"[NOT FOUND TEXTURE] Texture not found: {texture_path}"
+            print(message)
+            log_to_file(message)
 
 # Function to import .psk file
 def import_psk(file_path):
@@ -83,7 +98,8 @@ def clear_scene():
                 
 # Call the function to move .png files
 move_png_files(input_directory, output_directory)
-    
+
+texture_paths = []
 # Iterate through all .json files in the input mapping directory
 for root, dirs, files in os.walk(input_mapping_directory):
     for file in files:
@@ -101,29 +117,46 @@ for root, dirs, files in os.walk(input_mapping_directory):
             for key, value in data.items():
                 model_path = value.get("ModelPath", "")
                 skeleton_path = value.get("SkeletonPath", "")
+
+                # Iterate through Materials and grab texture paths
+                materials = value.get("Materials", {})
+                for material in materials.values():
+                    if isinstance(material, dict):
+                        textures = material.get("Textures", {})
+                        texture_paths.extend(textures.values())
                 
                 # Iterate through Accessories and grab model path
                 accessories = value.get("Accessories", [])
                 for accessory in accessories:
                     # Clear all objects
                     clear_scene()
+
+                    accessory_materials = accessory.get("Materials", {})
+                    for accessory_material in accessory_materials.values():
+                        if isinstance(accessory_material, dict):
+                            accessory_textures = accessory_material.get("Textures", {})
+                            texture_paths.extend(accessory_textures.values())
             
                     accessory_model_path = accessory.get("ModelPath", "")
                     accessory_glb_file_path = os.path.join(root_directory, "Output", accessory_model_path.replace('/assets/', ''))
                     accessory_model_path_psk = os.path.splitext(accessory_model_path)[0] + ".psk"
                     accessory_model_path_psk = accessory_model_path_psk.replace('/assets/', '')
                     accessory_psk_file_path = os.path.join(root_directory, "Input", accessory_model_path_psk)
+
+                    accessory_logs_check_glb_file_path = accessory_model_path.replace('/assets/', '')
+                    accessory_logs_check_file_path = os.path.join(log_check_file_path, accessory_logs_check_glb_file_path)
                     
                     # Skip if converted file already exists
                     if os.path.exists(accessory_glb_file_path):
-                        print(f"File {accessory_glb_file_path} already exists. Skipping export.")
+                        print(f"[SKIP] File {accessory_glb_file_path} already exists. Skipping export.")
                         continue
 
                     # Check if the file exists
                     if not os.path.exists(accessory_psk_file_path):
-                        log_message = f"File not found: {accessory_psk_file_path}. Skipping accessory."
+                        log_message = f"[NOT FOUND ACCESSORY] File not found: {accessory_psk_file_path}."
                         print(log_message)
-                        log_to_file(log_message)
+                        if not os.path.exists(accessory_logs_check_file_path):
+                            log_to_file(log_message)
                         continue
 
                     # Import the accessory PSK file
@@ -155,10 +188,12 @@ for root, dirs, files in os.walk(input_mapping_directory):
 
                 # Replace the extension with .psk
                 model_path_psk = os.path.splitext(model_path)[0] + ".psk"
+                model_path_glb = os.path.splitext(model_path)[0] + ".glb"
                 skeleton_path_psk = os.path.splitext(skeleton_path)[0] + ".psk"
 
                 # Remove /assets/ from path
                 model_path_psk = model_path_psk.replace('/assets/', '')
+                model_path_glb = model_path_glb.replace('/assets/', '')
                 skeleton_path_psk = skeleton_path_psk.replace('/assets/', '')
                 
                 output_model_path = model_path
@@ -167,18 +202,21 @@ for root, dirs, files in os.walk(input_mapping_directory):
                 
                 # Skip if converted file already exists
                 if os.path.exists(output_model_path):
-                    print(f"File {output_model_path} already exists. Skipping export.")
+                    print(f"[SKIP] File {output_model_path} already exists. Skipping export.")
                     continue
 
                 # Construct the full path for the PSK file
                 model_psk_file_path = os.path.join(root_directory, "Input", model_path_psk)
                 skeleton_psk_file_path = os.path.join(root_directory, "Input", skeleton_path_psk)
+
+                model_logs_check_file_path = os.path.join(log_check_file_path, model_path_glb)
                 
                 # If file doesnt exist, skip
                 if not os.path.exists(model_psk_file_path):
-                    log_message = f"File {model_psk_file_path} is not present. Skipping."
+                    log_message = f"[NOT FOUND MESH] File not found: {model_psk_file_path}"
                     print(log_message)
-                    log_to_file(log_message)
+                    if not os.path.exists(model_logs_check_file_path):
+                        log_to_file(log_message)
                     continue
                     
                 # Clear all objects
@@ -198,36 +236,15 @@ for root, dirs, files in os.walk(input_mapping_directory):
                             mesh_object = child
                             #print("Selected object is an armature. Found associated mesh.")
                             break
-                #else:
-                #    print("Selected object is not a mesh or armature with an associated mesh.")
-
-                # Clear parent relationship
-                #bpy.context.view_layer.objects.active = mesh_object
-                #bpy.ops.object.parent_clear(type='CLEAR')
-
-                # Delete the armature object
-                #bpy.data.objects.remove(selected_object)
-                #print("Armature deleted, only the mesh remains.")
 
                 # Select mesh and apply smooth shading
                 bpy.context.view_layer.objects.active = mesh_object
                 mesh_object.select_set(True)
                 bpy.ops.object.mode_set(mode='OBJECT')
                 bpy.ops.object.shade_smooth()
-                
-                # Load .psk file for mesh skeleton
-                # custom_armature_file_path = os.path.join(models_directory, dskeleton_file)
-                #import_psk(skeleton_psk_file_path)
-
-                # Select the mesh and armature objects
-                #bpy.context.view_layer.objects.active = mesh_object
-                #mesh_object.select_set(True)
-                #bpy.context.view_layer.objects.active = bpy.context.selected_objects[-1]
-                #bpy.context.selected_objects[-1].select_set(True)
-
-                # Parent the mesh to the armature with empty groups
-                #bpy.ops.object.parent_set(type='ARMATURE')
-
-                #print("Mesh parented to the armature with empty groups.")
 
                 export(model_path_psk)
+
+log_textures_to_file(texture_paths)
+
+print("Finished converting models.")
